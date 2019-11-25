4d78376dc7fec172d7f2081613fde058e4c31df2
Justin Zeyeum <justinzeyeum@yahoo.co.uk>
2019-11-26 00:30:30 +0200

    first commit

---
+++ userInput.py
@@ -0,0 +1,6 @@
+# A program asking user to input hours and rate per hour,
+# which display the pay on the screen
+hrs = input("Enter Hours:") # user input hours
+rate = input("Enter Rate:") # user input pay rate
+gross_pay = int(hrs) * float(rate) # program compute the gross pay calculation
+print("Pay:", gross_pay) # the gross pay is printed out as pay.
